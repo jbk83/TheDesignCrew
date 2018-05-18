7d802cfa61617a4021181b18a919ba921bc9806a
jbk83 <jbk_83@hotmail.com>
2018-05-18 15:04:34 +0200

    Fix design

--- app/assets/javascripts/components/scroll-navbar.js
+++ app/assets/javascripts/components/scroll-navbar.js
@@ -5,3 +5,18 @@ $(window).scroll((e) => {
       $(".js-scrollNavbar").removeClass("mod-boxshadow");
    }
 });
+
+$(() => {
+  $(".js-navbar").on("click", (event) => {
+    event.preventDefault();
+
+    let target = $(event.target.hash);
+    target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
+
+    if (target.length) {
+      $("html,body").stop().animate({
+        scrollTop: target.offset().top - $(".js-scrollspy").height()
+      }, 1000);
+    }
+  });
+});
